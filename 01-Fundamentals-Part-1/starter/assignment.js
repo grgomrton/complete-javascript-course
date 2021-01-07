79061a386c883a22fae997f4e4a71d43d7e02423
Márton Gergó <grgo.mrton@gmail.com>
2021-01-07 23:31:03 +0100

    assignment12

--- 01-Fundamentals-Part-1/starter/assignment.js
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -1,5 +1,15 @@
+// variables
 let country = "Hungary";
 let continent = "Europe";
 let population = 10;
 
 console.log("country", country, "continent", continent, "population", population);
+
+// data types
+let isIsland = false;
+let language;
+
+console.log("typeof isIsland", typeof isIsland);
+console.log("typeof population", typeof population);
+console.log("typeof country", typeof country);
+console.log("typeof language", typeof language);
