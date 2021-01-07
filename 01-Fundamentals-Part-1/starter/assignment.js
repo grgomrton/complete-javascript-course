3d2fe40028334d8063b2c85b9c851d4f5097399b
Márton Gergó <grgo.mrton@gmail.com>
2021-01-07 21:03:52 +0100

    lecture11 assigments add missing semicolon

--- 01-Fundamentals-Part-1/starter/assignment.js
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -2,4 +2,4 @@ let country = "Hungary";
 let continent = "Europe";
 let population = 10;
 
-console.log("country", country, "continent", continent, "population", population)
+console.log("country", country, "continent", continent, "population", population);
