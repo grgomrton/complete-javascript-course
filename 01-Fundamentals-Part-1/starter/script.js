dc0b0aaf4f35a3ad6fbe892f180965142eb54b2b
Márton Gergó <grgo.mrton@gmail.com>
2021-01-07 20:57:40 +0100

    video10

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,7 +1,10 @@
-let js = 'amazing';
-if (js === 'amazing') {
-  alert("JavaScript is FUN!");
-}
-
-40 + 8 + 23 - 10;
+let js = "amazing";
 console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Jonas";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
