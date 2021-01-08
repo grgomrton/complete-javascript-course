d4101e1c8eecf8ae06e38f0065899d8f934d291e
Márton Gergó <grgo.mrton@gmail.com>
2021-01-08 21:28:01 +0100

    assignment 13

--- 01-Fundamentals-Part-1/starter/assignment.js
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -1,15 +1,20 @@
+console.log("# Assignments");
 // variables
-let country = "Hungary";
-let continent = "Europe";
+const country = "Hungary";
+const continent = "Europe";
 let population = 10;
 
 console.log("country", country, "continent", continent, "population", population);
 
 // data types
-let isIsland = false;
+const isIsland = false;
 let language;
 
 console.log("typeof isIsland", typeof isIsland);
 console.log("typeof population", typeof population);
 console.log("typeof country", typeof country);
 console.log("typeof language", typeof language);
+
+// let, const, var
+language = "hungarian";
+// isIsland = true; // TypeError: Assignment to constant variable
