b8ebb8ac3788e760a050f10d9341db34772a8b9c
Márton Gergó <grgo.mrton@gmail.com>
2021-01-08 21:28:33 +0100

    video 14

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,10 +1,38 @@
-let js = "amazing";
+/*
+let js = 'amazing';
 console.log(40 + 8 + 23 - 10);
 
-console.log("Jonas");
+console.log('Jonas');
 console.log(23);
 
-let firstName = "Jonas";
+let firstName = 'Jonas';
 console.log(firstName);
 console.log(firstName);
 console.log(firstName);
+*/
+
+// Mathetmatical operators
+const currentYear = 2037;
+const jonasBirthYear = 1991;
+const sarahBirthYear = 2018;
+const ageJonas = currentYear - jonasBirthYear;
+const ageSarah = currentYear - sarahBirthYear;
+console.log('Jonas\' age', ageJonas);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+// Assignment operators
+const firstName = 'Jonas';
+const lastName = 'Schmedtmann';
+console.log(firstName + ' ' + lastName);
+
+let x = 10 + 5; // 15
+x += 10;
+x++;
+console.log(x);
+
+// Comparison operators
+console.log(ageJonas > ageSarah);
+
+console.log(currentYear - 1991 > currentYear - 2018);
