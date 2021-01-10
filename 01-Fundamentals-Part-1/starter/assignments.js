7808b252cefda3a6a9a8c3bcb8400f8051fc8667
Márton Gergó <grgo.mrton@gmail.com>
2021-01-10 21:38:27 +0100

    video 24

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,58 +1,68 @@
-// variables
-const country = "Hungary";
-const continent = "Europe";
-let population = 10;
+// // variables
+// const country = "Hungary";
+// const continent = "Europe";
+// let population = 10;
 
-console.log("country", country, "continent", continent, "population", population);
+// console.log("country", country, "continent", continent, "population", population);
 
-// data types
+// // data types
+// const isIsland = false;
+// let language;
+
+// console.log("typeof isIsland", typeof isIsland);
+// console.log("typeof population", typeof population);
+// console.log("typeof country", typeof country);
+// console.log("typeof language", typeof language);
+
+// // let, const, var
+// language = "hungarian";
+// // isIsland = true; // TypeError: Assignment to constant variable
+
+// // operators
+// const halfPopulation = population / 2;
+// console.log('1.', halfPopulation);
+// console.log('2.', population + 1);
+// const populationOfFinland = 6;
+// console.log('3.', population > populationOfFinland);
+// const avgCountryPopulation = 33;
+// console.log('4.', population < avgCountryPopulation);
+// const description = country + ' is in ' + continent + ', and its ' + population + ' million people speak ' + language;
+// console.log(description);
+
+// // strings
+// const descriptionTempLit = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
+// console.log(descriptionTempLit);
+
+// // if-else statements
+// if (population > 33) {
+//   console.log(`${country}'s population is above average`);
+// } else {
+//   console.log(`${country}'s population is ${33 - population} million below average`);
+// }
+
+// // type conversion, coercion
+// console.log('9' - '5'); // will be 4
+// console.log('19' - '13' + '17'); // will be 617
+// console.log('19' - '13' + 17); // will be 23
+// console.log('123' < 57); // will be false
+// console.log(5 + 6 + '4' + 9 - 4 - 2); // will be 1143
+
+// // equality operators
+// const numNeighbours = Number(prompt('How many neighbour countries does your country have?'));
+// if (numNeighbours === 1) {
+//   console.log('Only 1 border!');
+// } else if (numNeighbours > 1) {
+//   console.log('More than one border');
+// } else {
+//   console.log('No borders');
+// }
+
+const englishSpeaking = false;
+const population = 10;
 const isIsland = false;
-let language;
 
-console.log("typeof isIsland", typeof isIsland);
-console.log("typeof population", typeof population);
-console.log("typeof country", typeof country);
-console.log("typeof language", typeof language);
-
-// let, const, var
-language = "hungarian";
-// isIsland = true; // TypeError: Assignment to constant variable
-
-// operators
-const halfPopulation = population / 2;
-console.log('1.', halfPopulation);
-console.log('2.', population + 1);
-const populationOfFinland = 6;
-console.log('3.', population > populationOfFinland);
-const avgCountryPopulation = 33;
-console.log('4.', population < avgCountryPopulation);
-const description = country + ' is in ' + continent + ', and its ' + population + ' million people speak ' + language;
-console.log(description);
-
-// strings
-const descriptionTempLit = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
-console.log(descriptionTempLit);
-
-// if-else statements
-if (population > 33) {
-  console.log(`${country}'s population is above average`);
+if (englishSpeaking && (population < 50) && !isIsland) {
+  console.log('You should live in Hungary!');
 } else {
-  console.log(`${country}'s population is ${33 - population} million below average`);
-}
-
-// type conversion, coercion
-console.log('9' - '5'); // will be 4
-console.log('19' - '13' + '17'); // will be 617
-console.log('19' - '13' + 17); // will be 23
-console.log('123' < 57); // will be false
-console.log(5 + 6 + '4' + 9 - 4 - 2); // will be 1143
-
-// equality operators
-const numNeighbours = Number(prompt('How many neighbour countries does your country have?'));
-if (numNeighbours === 1) {
-  console.log('Only 1 border!');
-} else if (numNeighbours > 1) {
-  console.log('More than one border');
-} else {
-  console.log('No borders');
+  console.log('Hungary does not meet your criteria.');
 }
