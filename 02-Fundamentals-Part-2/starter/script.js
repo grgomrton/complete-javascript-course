69ccdec7b9c8e391722909c7174718b29cba17c3
Márton Gergó <grgo.mrton@gmail.com>
2021-01-13 15:26:49 +0100

    video 32

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,14 @@
+// strict mode
+'use strict';
+
+let hasDriversLicence = false;
+const passTest = true;
+
+if (passTest) {
+  hasDriverLicense = true;
+}
+if (hasDriversLicence) {
+  console.log('I can drive!');
+}
+
+const interface = 'Audio';
