262c28bfda1fc69be68fb136786f4db44b42105e
Márton Gergó <grgo.mrton@gmail.com>
2021-01-13 21:25:46 +0100

    video 34

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -1,11 +1,32 @@
-function describeCountry(country, population, capitalCity) {
-  return `${country} has ${population} million people and its capital city is ${capitalCity}`;
+// // functions
+// function describeCountry(country, population, capitalCity) {
+//   return `${country} has ${population} million people and its capital city is ${capitalCity}`;
+// }
+
+// const austria = describeCountry('Austria', 9, 'Vienna');
+// const hungary = describeCountry('Hungary', 10, 'Budapest');
+// const portugal = describeCountry('Portugal', 10, 'Lisbon');
+
+// console.log(austria);
+// console.log(hungary);
+// console.log(portugal);
+
+const entirePopulation = 7900;
+
+function percentageOfWorld1(population) {
+  return population / entirePopulation * 100;
 }
 
-const austria = describeCountry('Austria', 9, 'Vienna');
-const hungary = describeCountry('Hungary', 10, 'Budapest');
-const portugal = describeCountry('Portugal', 10, 'Lisbon');
+const austriaPopulation = 9;
+const hungaryPopulation = 10;
+const usaPopulation = 328;
+console.log('Austria', percentageOfWorld1(austriaPopulation));
+console.log('Hungary', percentageOfWorld1(hungaryPopulation));
+console.log('USA', percentageOfWorld1(usaPopulation));
 
-console.log(austria);
-console.log(hungary);
-console.log(portugal);
+const percentageOfWorld2 = function (population) {
+  return population / entirePopulation * 100;
+}
+console.log('Austria', percentageOfWorld2(austriaPopulation));
+console.log('Hungary', percentageOfWorld2(hungaryPopulation));
+console.log('USA', percentageOfWorld2(usaPopulation));
--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -13,23 +13,39 @@
 
 // const interface = 'Audio';
 
-// functions
-function logger() {
-  console.log('My name is Jonas');
+// // functions
+// function logger() {
+//   console.log('My name is Jonas');
+// }
+
+// logger();
+// const nonRetValue = logger();
+// console.log(nonRetValue);
+
+// function fruitProcessor(apples, oranges) {
+//   const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+//   return juice;
+// }
+
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
+
+// const appleOrangeJuice = fruitProcessor(2, 4);
+// console.log(appleOrangeJuice);
+
+const now = 2037;
+
+// function declaration
+function calcAge1(birthYear) {
+  return now - birthYear;
 }
+const age1 = calcAge1(1991);
 
-logger();
-const nonRetValue = logger();
-console.log(nonRetValue);
-
-function fruitProcessor(apples, oranges) {
-  const juice = `Juice with ${apples} apples and ${oranges} oranges`;
-  return juice;
+// function expression
+const calcAge2 = function (birthYear) {
+  return now - birthYear;
 }
+const age2 = calcAge2(1991);
 
-const appleJuice = fruitProcessor(5, 0);
-console.log(appleJuice);
-
-const appleOrangeJuice = fruitProcessor(2, 4);
-console.log(appleOrangeJuice);
-
+console.log(age1);
+console.log(age2);
