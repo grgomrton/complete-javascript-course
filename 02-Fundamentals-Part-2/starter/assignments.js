41f949bf3564da1bad4ba2c40d6690b2665ea211
Márton Gergó <grgo.mrton@gmail.com>
2021-01-14 19:14:39 +0100

    video 36

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -11,27 +11,41 @@
 // console.log(hungary);
 // console.log(portugal);
 
-const entirePopulation = 7900;
+// const entirePopulation = 7900;
 
+// function percentageOfWorld1(population) {
+//   return population / entirePopulation * 100;
+// }
+
+// const austriaPopulation = 9;
+// const hungaryPopulation = 10;
+// const usaPopulation = 328;
+// console.log('Austria', percentageOfWorld1(austriaPopulation));
+// console.log('Hungary', percentageOfWorld1(hungaryPopulation));
+// console.log('USA', percentageOfWorld1(usaPopulation));
+
+// const percentageOfWorld2 = function (population) {
+//   return population / entirePopulation * 100;
+// }
+// console.log('Austria', percentageOfWorld2(austriaPopulation));
+// console.log('Hungary', percentageOfWorld2(hungaryPopulation));
+// console.log('USA', percentageOfWorld2(usaPopulation));
+
+// const percentageOfWorld3 = population => population / entirePopulation * 100;
+// console.log('Austria', percentageOfWorld3(austriaPopulation));
+// console.log('Hungary', percentageOfWorld3(hungaryPopulation));
+// console.log('USA', percentageOfWorld3(usaPopulation));
+
+// function calls from other functions
+const entirePopulation = 7900;
 function percentageOfWorld1(population) {
   return population / entirePopulation * 100;
 }
 
-const austriaPopulation = 9;
-const hungaryPopulation = 10;
-const usaPopulation = 328;
-console.log('Austria', percentageOfWorld1(austriaPopulation));
-console.log('Hungary', percentageOfWorld1(hungaryPopulation));
-console.log('USA', percentageOfWorld1(usaPopulation));
-
-const percentageOfWorld2 = function (population) {
-  return population / entirePopulation * 100;
+const describePopulation = (country, population) => {
+  `${country} has ${population} million people, which is about ${percentageOfWorld1(population)}% of the world.`
 }
-console.log('Austria', percentageOfWorld2(austriaPopulation));
-console.log('Hungary', percentageOfWorld2(hungaryPopulation));
-console.log('USA', percentageOfWorld2(usaPopulation));
 
-const percentageOfWorld3 = population => population / entirePopulation * 100;
-console.log('Austria', percentageOfWorld3(austriaPopulation));
-console.log('Hungary', percentageOfWorld3(hungaryPopulation));
-console.log('USA', percentageOfWorld3(usaPopulation));
+console.log('Austria', percentageOfWorld1(9));
+console.log('Hungary', percentageOfWorld1(10));
+console.log('USA', percentageOfWorld1(328));
--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -50,17 +50,32 @@
 // console.log(age1);
 // console.log(age2);
 
-// arrow function
-const calcAge3 = birthYear => 2037 - birthYear;
-const age3 = calcAge3(1991);
+// // arrow function
+// const calcAge3 = birthYear => 2037 - birthYear;
+// const age3 = calcAge3(1991);
 
-console.log(age3);
+// console.log(age3);
 
-const yearsUntilRetirement = (birthYear, firstName) => {
-  const age = 2037 - birthYear;
-  const retirement = 65 - age;
-  return `${firstName} retires in ${retirement} years`;
+// const yearsUntilRetirement = (birthYear, firstName) => {
+//   const age = 2037 - birthYear;
+//   const retirement = 65 - age;
+//   return `${firstName} retires in ${retirement} years`;
+// }
+
+// console.log(yearsUntilRetirement(1991, 'Jonas'));
+// console.log(yearsUntilRetirement(1980, 'Bob'));
+
+// function calls from other functions
+function cutFruitPieces(fruit) {
+  return fruit * 4;
 }
 
-console.log(yearsUntilRetirement(1991, 'Jonas'));
-console.log(yearsUntilRetirement(1980, 'Bob'));
+function fruitProcessor(apples, oranges) {
+  const applePieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
+
+  const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange`;
+  return juice;
+}
+
+console.log(fruitProcessor(2, 3));
