763d5ff4f154872d2ab4e8a3ebe772054c0aa8b3
Márton Gergó <grgo.mrton@gmail.com>
2021-01-30 08:51:55 +0100

    video 79

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -4,6 +4,7 @@ const generateSecretNumber = () => Math.trunc(Math.random() * 20) + 1;
 
 let secretNumber = generateSecretNumber();
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.btn.again').addEventListener('click', () => {
   secretNumber = generateSecretNumber();
@@ -27,6 +28,10 @@ document.querySelector('.btn.check').addEventListener('click', () => {
         document.querySelector('.number').textContent = secretNumber;
         document.querySelector('body').style.backgroundColor = '#60b347';
         document.querySelector('.number').style.width = '30rem';
+        if (score > highscore) {
+          highscore = score;
+          document.querySelector('.highscore').textContent = highscore;
+        }
       } else if (guess > secretNumber) {
         messageBox.textContent = 'Too high';
         score -= 1;
