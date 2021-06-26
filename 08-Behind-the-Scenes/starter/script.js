7085af72be489c8893d63a8da06aa2d68601487a
Márton Gergó <grgo.mrton@gmail.com>
2021-06-26 17:26:05 +0200

    variable scopes

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,28 @@
-'use strict';
+//'use strict';
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    const output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    console.log(output);
+  }
+
+  if (birthYear >= 1981 && birthYear <= 1996) {
+    var millenial = true;
+    const notification = `Oh, and your a millenial, ${firstName}`;
+    console.log(notification);
+
+    function add(a, b) {
+      return a + b;
+    }
+  }
+  console.log(add(1, 2));
+  console.log(millenial);
+  printAge();
+
+  return age;
+}
+
+const firstName = 'Jonas';
+calcAge(1991);
